e8e3f90d8cf1d73247131e7a5eb94f2725bf7748
Administrator <ds02.ho@lippoinsurance.com>
2024-03-13 14:40:23 +0700

    view detail on realization -> replace '/' to '~'

--- resources/views/pages/realization/show.blade.php
+++ resources/views/pages/realization/show.blade.php
@@ -183,7 +183,7 @@
                             </div>
                             <div class="row">
                                 <div class="col-lg-12 mb-2">
-                                    <a href="{{ route('realization.detail-realization.index', $RealizationData->Invoice_No) }}" style="width: 100%; height: 60px;" class="btn btn-outline-primary radius-100">
+                                    <a href="{{ route('realization.detail-realization.index', str_replace('/', '~', $RealizationData->Invoice_No)) }}" style="width: 100%; height: 60px;" class="btn btn-outline-primary radius-100">
                                         <b style="font-size: 18px;">View Detail</b>
                                     </a>
                                     <a href="{{ route('realization.index') }}" style="width: 100%; height: 60px;" class="btn btn-primary radius-100 mt-2">
