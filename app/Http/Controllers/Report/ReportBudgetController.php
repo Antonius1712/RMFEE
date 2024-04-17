cc04b71659959ab1fcbb5d3d0661fff7b399c605
Administrator <ds02.ho@lippoinsurance.com>
2024-04-17 15:31:24 +0700

    add validation and notification on report budget

--- app/Http/Controllers/Report/ReportBudgetController.php
+++ app/Http/Controllers/Report/ReportBudgetController.php
@@ -24,14 +24,14 @@ class ReportBudgetController extends Controller
     public function GenerateReport(Request $request){
         
         //? start date null.
-        if($request->input('start_date') == null) {
+        if(empty($request->input('start_date'))) {
             $start_date = date('Y-m-01');
         }else{
             $start_date = date('Y-m-d', strtotime($request->input('start_date')));
         }
         
         //? enddate null.
-        if($request->input('end_date') == null) {
+        if(empty($request->input('end_date'))) {
             $end_date = date('Y-m-t');
         }else{
             $end_date = date('Y-m-d', strtotime($request->input('end_date')));
@@ -52,65 +52,70 @@ class ReportBudgetController extends Controller
     public function ExportBudgetSumarry($start_date, $end_date, $status_budget = ''){
         $data = [];
         $Budgets = Budget::GetReportBudgetSummary($start_date, $end_date, $status_budget);
-        //? loop rows.
-        foreach( $Budgets as $row => $result ){
-            //? loop columns and value for each column.
-            foreach( collect($result)->toArray() as $header => $value ){
-                if($header == 'LGI_PREMIUM' || $header == 'PREMIUM' || $header == 'ADMIN' || $header == 'DISCOUNT' || $header == 'OTHERINCOME' || $header == 'PAYMENT' || $header == 'OS' || $header == 'BUDGET' || $header == 'REALIZATION_RMF' || $header == 'REALIZATION_SPONSORSHIP' || $header == 'REMAIN_BUDGET') {
-                    // Set cell value explicitly as a number
-                    $data[$row][$header] = (float) $value;
-                } elseif($header == 'START_DATE' || $header == 'END_DATE' || $header == 'BOOKING_DATE') {
-                    $data[$row][$header] = date('d M Y', strtotime($value));
-                } else {
-                    $data[$row][$header] = $value;
+        
+        if (!empty($Budgets)) {
+            //? loop rows.
+            foreach( $Budgets as $row => $result ){
+                //? loop columns and value for each column.
+                foreach( collect($result)->toArray() as $header => $value ){
+                    if($header == 'LGI_PREMIUM' || $header == 'PREMIUM' || $header == 'ADMIN' || $header == 'DISCOUNT' || $header == 'OTHERINCOME' || $header == 'PAYMENT' || $header == 'OS' || $header == 'BUDGET' || $header == 'REALIZATION_RMF' || $header == 'REALIZATION_SPONSORSHIP' || $header == 'REMAIN_BUDGET') {
+                        // Set cell value explicitly as a number
+                        $data[$row][$header] = (float) $value;
+                    } elseif($header == 'START_DATE' || $header == 'END_DATE' || $header == 'BOOKING_DATE') {
+                        $data[$row][$header] = date('d M Y', strtotime($value));
+                    } else {
+                        $data[$row][$header] = $value;
+                    }
                 }
             }
+
+            // dd($data);
+            $formatedStartDate = date('d M Y', strtotime($start_date));
+            $formatedEndDate = date('d M Y', strtotime($end_date));
+            Excel::create('Budget Summary ('.$formatedStartDate.' - '.$formatedEndDate.')', function($excel) use($data){
+                $excel->sheet('Budget Summary', function($sheet) use ($data) {
+                    $sheet->freezeFirstRow();
+                    $sheet->setAllBorders('thin');
+
+                    // Styling Cell A1 - AJ1.
+                    $sheet->getStyle('A1:F1')->applyFromArray([
+                        'font' => [
+                            'name'      =>  'Calibri',
+                            'size'      =>  15,
+                            'bold'      =>  true,
+                            'align'     =>  'center'
+                        ],
+                        'fill' => [
+                            'type'  => PHPExcel_Style_Fill::FILL_SOLID,
+                            'color' => array('rgb' => 'FFF200')
+                        ],
+                        'alignment' => [
+                            'horizontal' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
+                            'vertical' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER
+                        ]
+                    ]);
+                    
+                    // Set the size for cells.
+                    $sheet->setSize('A1:F1', '', 50);
+                    
+                    // Set the data.
+                    $sheet->fromArray($data);
+
+                    // Apply number format for columns declare below start from 2 to end.
+                    $lastRow = count($data) + 1;
+                    for ($row = 2; $row <= $lastRow; $row++) {
+                        $sheet->getStyle('E' . $row . ':F' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+                    }
+
+                    // Auto-size columns.
+                    foreach(range('A','F') as $column) {
+                        $sheet->getColumnDimension($column)->setAutoSize(true);
+                    }
+                });
+            })->export('xls')->download('xls');
+        }else{
+            return redirect()->back()->with(['status' => 404, 'notification' => 'Empty Data.']);
         }
-
-        // dd($data);
-        $formatedStartDate = date('d M Y', strtotime($start_date));
-        $formatedEndDate = date('d M Y', strtotime($end_date));
-        Excel::create('Budget Summary ('.$formatedStartDate.' - '.$formatedEndDate.')', function($excel) use($data){
-            $excel->sheet('Budget Summary', function($sheet) use ($data) {
-                $sheet->freezeFirstRow();
-                $sheet->setAllBorders('thin');
-
-                // Styling Cell A1 - AJ1.
-                $sheet->getStyle('A1:F1')->applyFromArray([
-                    'font' => [
-                        'name'      =>  'Calibri',
-                        'size'      =>  15,
-                        'bold'      =>  true,
-                        'align'     =>  'center'
-                    ],
-                    'fill' => [
-                        'type'  => PHPExcel_Style_Fill::FILL_SOLID,
-                        'color' => array('rgb' => 'FFF200')
-                    ],
-                    'alignment' => [
-                        'horizontal' => PHPExcel_Style_Alignment::VERTICAL_CENTER,
-                        'vertical' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER
-                    ]
-                ]);
-                
-                // Set the size for cells.
-                $sheet->setSize('A1:F1', '', 50);
-                
-                // Set the data.
-                $sheet->fromArray($data);
-
-                // Apply number format for columns declare below start from 2 to end.
-                $lastRow = count($data) + 1;
-                for ($row = 2; $row <= $lastRow; $row++) {
-                    $sheet->getStyle('E' . $row . ':F' . $row)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
-                }
-
-                // Auto-size columns.
-                foreach(range('A','F') as $column) {
-                    $sheet->getColumnDimension($column)->setAutoSize(true);
-                }
-            });
-        })->export('xls')->download('xls');
     }
 
     public function ExportBudgetDetail($start_date, $end_date, $status_budget = ''){
